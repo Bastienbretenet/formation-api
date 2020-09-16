dc628ce4e3ca0d5d451c9c4a7ff794172d464baf
Bastien <bastienbretenet@gmail.com>
2020-09-16 15:52:06 +0200

    Invoices Page

--- assets/js/pages/InvoicesPage.jsx
+++ assets/js/pages/InvoicesPage.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import Pagination from '../components/Pagination';
 import InvoicesAPI from '../services/invoicesAPI';
-import axios from 'axios';
 import moment from 'moment';
 
 const STATUS_CLASSES = {
